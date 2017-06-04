764d9c5672d8d6e6bccca03229b83dea870e2296
goncaloruas <salo.ruas@gmail.com>
2017-06-04 17:14:31 +0100

    added new controller

---
+++ views/crops/index.php
@@ -0,0 +1,18 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+/* @var $this yii\web\View */
+
+$this->title = 'NCrops';
+?>
+<div class="site-index">
+
+    <div class="body-content">
+
+        <div class="row">
+           
+            <h1>Crops Index</h1> 
+        </div>
+      
+        </div>
+</div>
